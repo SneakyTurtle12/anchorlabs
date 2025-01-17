45500412183f39f5c380bd9cf8bc6af38477774c
roe-cress <carolinehcress@gmail.com>
2025-01-17 13:05:48 +0200

    :(

--- play_around.py
+++ play_around.py
@@ -7,8 +7,10 @@ import json
 import plotly.express as px
 import streamlit_vertical_slider as svs
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import altair as alt
 
-st.set_page_config(layout='wide')
+st.set_page_config(layout='wide', page_title='Kenya Waterbasin Investment Dashboard PLAY AROUND')
 input_weighting = st.container(border=True)
 output_graph = st.container(border=True)
 
@@ -30,16 +32,15 @@ with input_weighting:
         with sliders:
             s_1, s_2, s_3, s_4, s_5 = st.columns(5)
             with s_1:
-                roi_weight = svs.vertical_slider(label="ROI weight", default_value=0, min_value=0, max_value=100, track_color="#00AAFF", slider_color="#00FFAA", thumb_color="#00FFAA", value_always_visible=True)
+                roi_weight = svs.vertical_slider(label="ROI weight", default_value=0, min_value=0, max_value=100, track_color="#41448788", slider_color="#414487FF", thumb_color="#414487FF", value_always_visible=True)
             with s_2:
-                pop_dense_weight = svs.vertical_slider(label="Population density weight", default_value=0, min_value=0, max_value=100, track_color="#00AAFF", slider_color="#00FFAA", thumb_color="#00FFAA", value_always_visible=True)
+                pop_dense_weight = svs.vertical_slider(label="Population density weight", default_value=0, min_value=0, max_value=100, track_color="#2A788E88", slider_color="#2A788EFF", thumb_color="#2A788EFF", value_always_visible=True)
             with s_3:
-                pov_distribute_weight = svs.vertical_slider(label="Poverty distribution weight", default_value=0, min_value=0, max_value=100, track_color="#00AAFF", slider_color="#00FFAA", thumb_color="#00FFAA", value_always_visible=True)
+                pov_distribute_weight = svs.vertical_slider(label="Poverty distribution weight", default_value=0, min_value=0, max_value=100, track_color="#22A88488", slider_color="#22A884FF", thumb_color="#22A884FF", value_always_visible=True)
             with s_4:
-                bio_diverse_weight = svs.vertical_slider(label="Bio-diversity weight", default_value=0, min_value=0, max_value=100, track_color="#00AAFF", slider_color="#00FFAA", thumb_color="#00FFAA", value_always_visible=True)
+                bio_diverse_weight = svs.vertical_slider(label="Bio-diversity weight", default_value=0, min_value=0, max_value=100, track_color="#7AD15188", slider_color="#7AD151FF", thumb_color="#7AD151FF", value_always_visible=True)
             with s_5:
-                climate_vul_weight = svs.vertical_slider(label="Climate vulnerability weight", default_value=0, min_value= 0, max_value=100, track_color="#00AAFF", slider_color="#00FFAA", thumb_color="#00FFAA", value_always_visible=True)
-            st.write(str(roi_weight)+ ' ' + str(pop_dense_weight))
+                climate_vul_weight = svs.vertical_slider(label="Climate vulnerability weight", default_value=0, min_value= 0, max_value=100, track_color="#FDE72588", slider_color="#FDE725FF", thumb_color="#FDE725FF", value_always_visible=True)
             weighted_score = roi_score*roi_weight + pop_dense*pop_dense_weight + pov_distribute*pov_distribute_weight + bio_diverse*bio_diverse_weight + climate_vul*climate_vul_weight
             max_score = max(weighted_score)
             weighted_score = (weighted_score/max_score)*100
@@ -57,18 +58,33 @@ with input_weighting:
                 aspects_weighted_data = [['ROI', roi_weight],['Population density', pop_dense_weight],['Poverty Distribution', pov_distribute_weight],['Bio-diversity', bio_diverse_weight],['Climate vulnerability', climate_vul_weight]]
                 aspects_weighted_df = pd.DataFrame(aspects_weighted_data)
                 aspects_weighted_df.columns = ['Aspect', 'Weighting']
-                pie_c = px.pie(aspects_weighted_df, values='Weighting', names='Aspect', title='Your chosen weightings of each aspect')
-                st.plotly_chart(pie_c, theme='streamlit', use_container_width=True)
+                palette = ['#414487FF','#2A788EFF', '#22A884FF', '#7AD151FF', '#FDE725FF']
+                pie_c = go.Figure(
+                    data=[
+                        go.Pie(
+                            labels=aspects_weighted_df['Aspect'],
+                            marker={'colors':palette},                                
+                            values=aspects_weighted_df['Weighting'],
+                        )
+                    ]
+                )
+                pie_c.update_traces(
+                    hoverinfo='label+percent',
+                    textinfo='percent',
+                    textfont_size=15
+                )
+
+                #pie_c = px.pie(aspects_weighted_df, values='Weighting', names='Aspect', title='Your chosen weightings of each aspect', color_discrete_map={'ROI': 'green', 'Population density': 'orange', 'Poverty Distribution': '#AAFF00', 'Bio-diversity': '#AA00FF','Climate vulnerability':'#FF00AA'})
+                st.plotly_chart(pie_c, use_container_width=True)
 
 
 
 
-#123skylar was here
+
 
 with output_graph:
     out_left_col, out_right_col = st.columns(2)
     with out_left_col:
-        #weighted_df = pd.read_csv('Basin_value.csv')
         shapefile= gpd.read_file('WRA_SubBasins_v2_added_variables.shp')
         shapefile = shapefile.merge(weighted_df, on='Basin_ID2')
         fig, ax = plt.subplots(1, figsize=(10, 6))
